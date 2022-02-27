55c415fa30601df39916a9353b94bc92a7e1ee13
Lisa Bergström <lisa.bergstrom@live.se>
2022-02-27 11:41:21 +0100

    answer

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,17 @@
 export const hackerSpeak = (words) => {
   return words
-}
+    .replace(/a/gi, "4")
+    .replace(/e/gi, "3")
+    .replace(/i/gi, "1")
+    .replace(/o/gi, "0")
+    .replace(/s/gi, "5");
+};
+
+export const hackerSpeak = (words) => {
+  words = words.replace(
+    /[aeios]/g,
+    (charactersToReplace) =>
+      ({ a: "4", e: "3", i: "1", o: "0", s: "5" }[charactersToReplace])
+  );
+  return words;
+};
--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,3 +1,8 @@
 export const hashtags = (text) => {
-  return text
+  return  text.split (''); filter(word => word.substring(0,1) ==="#");
+
+}
+
+export const hashtags = (text) => {
+  return text.split(' ').filter(word => word.startsWith('#'))
 }
